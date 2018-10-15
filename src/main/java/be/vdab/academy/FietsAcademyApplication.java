61759530b29d273283e464b336fb019658e65fb0
Stijn Verholen <stijn.verholen@vdabcampus.be>
2018-10-15 15:15:30 +0200

    Initial Spring Boot project

---
+++ src/main/java/be/vdab/academy/FietsAcademyApplication.java
@@ -0,0 +1,12 @@
+package be.vdab.academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FietsAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FietsAcademyApplication.class, args);
+	}
+}
