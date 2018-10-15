bd2b960fc34d5ab23e39cbfeb37c2198ff6994d0
Stijn Verholen <stijn.verholen@vdabcampus.be>
2018-10-15 15:17:45 +0200

    Added DataSourceTest

---
+++ src/test/java/be/vdab/academy/repositories/DataSourceTest.java
@@ -0,0 +1,19 @@
+package be.vdab.academy.repositories;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+public class DataSourceTest {
+
+	@Test
+	public void initialisesOK() {
+		
+	}
+}
